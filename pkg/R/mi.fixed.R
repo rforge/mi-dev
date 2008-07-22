a434adc2414443ef98e1ab0f682b5ed4074020ab
masanao <masanao@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-22 20:35:42 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mi.fixed.R
+++ pkg/R/mi.fixed.R
@@ -71,23 +71,3 @@ mi.copy <- function( Y, X, ... ) {
   return( result )
   on.exit( rm( fixd.imp) )
 }
-
-setMethod("mi.plot", signature(object = "mi.copy"), 
- function ( object, Yobs, main=deparse( substitute( Yobs ) ), gray.scale = FALSE ) {
-  #par(mfrow=c(1,4))
-#  fit     <- mi.expected( object )
-#  res     <- mi.resid( object, Yobs )
-#  sigma   <- mi.sigma(object)
-#  vrb.obs <- Yobs
-#  vrb.imp <- mi.imputed( object, Yobs )
-#  fit1 <- fit[[1]]
-#  fit2 <- fit[[2]]
-#  res1 <- res[[1]]
-#  res2 <- res[[2]]    
-#  mi.hist ( vrb.obs, object, xlab = main, main = main, gray.scale = gray.scale )
-#  binnedplot( fit1[ !is.na( Yobs ) ], res1[ !is.na( Yobs ) ], nclass=sqrt(length(fit1[ !is.na( Yobs ) ])), main = main)
-#  residual.plot ( fit2, res2, sigma, main = main, gray.scale = gray.scale )
-#  #mtext( "sqrt", 2, cex = 0.7, adj = 1 )
-#  mi.scatterplot ( vrb.obs, vrb.imp, xlab = "predicted", ylab = main, main = main, gray.scale = gray.scale, display.zero=FALSE )
-}
-)
