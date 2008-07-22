447c120ff52e4bfa98289d641082679efa3e6f7c
masanao <masanao@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-22 21:21:19 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/imputed.R
+++ pkg/R/imputed.R
@@ -1,9 +1,15 @@
+# ==============================================================================
+# extract imputed values for mi.method class object
+# ==============================================================================
 setMethod( "imputed", signature( object = "mi.method" ),     
   function ( object, Y ) {
     Y[is.na( Y )] <- object$random
     return( Y )
   }
 )
+# ==============================================================================
+# extract imputed values for mi.mixed class object
+# ==============================================================================
 setMethod("imputed", signature(object = "mi.mixed"),     
   function ( object, Y ) {
     Y[ is.na( Y ) ] <- object$random
