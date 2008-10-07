66c46148b7d0950e814fafadc5ca319385bbd35c
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-07 16:33:38 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glm.mi.R
+++ pkg/R/glm.mi.R
@@ -32,27 +32,3 @@ glm.mi <- function ( formula, mi.object, family = gaussian, ... )
     class( mi.glm.object ) <- c( "mi.glm", "list" );
     return( mi.glm.object );
 }
-
-print.mi.glm <- function(x, ...){
-  display(x, ...)
-}
-
-
-setMethod("display", signature(object = "mi.glm"),     
-    function ( object, digits=2 ) {
-  cat( "=======================================\n" );
-  cat( "Pooled Estimate\n" );
-  cat( "=======================================\n" );
-  print( object$call, digits=digits );
-  tab <- cbind( object$glm.mi.pooled[[1]], object$glm.mi.pooled[[2]] );
-  dimnames( tab )[[2]] <- c( "coef.est", "coef.se" );
-  pfround( tab,  digits=digits);
-  cat( "\n=======================================\n" );
-  cat( "Separate Estimate for each Imputation\n" );
-  cat( "=======================================\n" );
-  for( i in 1:length( object$glm.mi.fit ) ){
-    cat( "\n** Imputation", i, "**\n" );
-    display( object$glm.mi.fit[[i]], digits=digits );
-  }
-}
-)
--- pkg/R/lm.mi.R
+++ pkg/R/lm.mi.R
@@ -30,29 +30,8 @@ lm.mi <- function ( formula,  mi.object, ... )
     return( mi.lm.object );
 }
 
-print.mi.lm <- function(x, ...){
-  display(x, ...)
-}
 
 
-setMethod("display", signature(object = "mi.lm"),     
-    function ( object, digits=2 ) {
-  cat( "=======================================\n" );
-  cat( "Pooled Estimate\n" );
-  cat( "=======================================\n" );
-  print( object$call, digits=digits );
-  tab <- cbind( object$lm.mi.pooled[[1]], object$lm.mi.pooled[[2]] );
-  dimnames( tab )[[2]] <- c( "coef.est", "coef.se" );
-  pfround( tab,  digits=digits);
-  cat( "\n=======================================\n" );
-  cat( "Separate Estimate for each Imputation\n" );
-  cat( "=======================================\n" );
-  for( i in 1:length( object$lm.mi.fit ) ){
-    cat( "\n** Imputation", i, "**\n" );
-    display( object$lm.mi.fit[[i]], digits=digits );
-  }
-}
-)
 
 
 
