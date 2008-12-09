2fe7c3687dd4203cbba77c428fc6c50f546dde2a
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-09 04:54:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@163 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mi.dichotomous.R
+++ pkg/R/mi.dichotomous.R
@@ -40,7 +40,7 @@ mi.dichotomous <- function( formula, data = NULL, start = NULL, n.iter = 100,
   if(augment.data){
     data2 <- rbind.data.frame(data, .randdraw(data, n=trunc(dim(data)[1])*0.1))
     bglm.imp <- bayesglm( formula = formula, 
-                          data = .data.aug(data, n=trunc(dim(data)[1]*0.1)), 
+                          data = data2, 
                           family = binomial ( link = "logit" ), 
                           n.iter = n.iter, start = start, 
                           drop.unused.levels=FALSE, Warning=FALSE, ... )
