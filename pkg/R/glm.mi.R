b3ad600c1a1a93ea5dec8c65f4a5b8f2e06d8f1f
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-07 15:37:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glm.mi.R
+++ pkg/R/glm.mi.R
@@ -20,18 +20,10 @@ glm.mi <- function (formula, mi.object, family = gaussian, ... )
       coef[[j]]<- lapply( result, summary )[[ j ]]$coef[ ,1]
       se[[j]]  <- lapply( result, summary )[[ j ]]$coef[ ,2]
     }
-    W         <- colSums( do.call( rbind, se )^2 ) / m
-    Bhat      <- colSums( do.call( rbind, coef ) ) / m
-    Bhat_rep  <- t( matrix( rep( Bhat, m ), length( Bhat ), m ) )
-    B         <- colSums( do.call( rbind, coef ) - Bhat_rep ) ^ 2 / ( m - 1 )
-    
-    pooled <- list( coefficients = NULL, se = NULL )
-    pooled$coefficients <- Bhat
-    pooled$se <- sqrt( W + ( 1 + 1 / m ) * B )
-    
-    mi.glm.object <- new("mi.glm",
+    pooled <- mi.pooled(coef, se, m)
+    mi.pooled.object <- new("mi.pooled",
                           call = call, 
                           mi.pooled = pooled,
-                          mi.fit = result )
-    return( mi.glm.object )
+                          mi.fit = result)
+    return( mi.pooled.object )
 }
