1cb4a28b8a4784c2f27f6dcb2b363d400ec10f55
yusung <yusung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-11 06:18:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mi-dev@326 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mi.pooled.R
+++ pkg/R/mi.pooled.R
@@ -4,7 +4,7 @@ mi.pooled <- function(coef, se, m){
     #Bhat_rep  <- t( matrix( rep( Bhat, m ), length( Bhat ), m ) )
     B <- apply(apply(do.call(rbind, coef), 1,  "-", Bhat), 1, var)
     #B         <- colSums( (do.call( rbind, coef ) - Bhat_rep)^ 2) / ( m - 1 )
-    pooled <- list( coefficients = NULL, se = NULL )
+    pooled <- list(coefficients = NULL, se = NULL)
     pooled$coefficients <- Bhat
     pooled$se <- sqrt(W+(1+1/m)*B)
     return(pooled)
